95944a3faf5f8acd1fd968e533f59415974eba7c
cain.chu <cainmaila>
2024-06-19 17:38:48 +0800

    Tifa聊天

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,72 @@
 <script lang="ts">
-	import ollama from 'ollama/browser'
-	import { onMount } from 'svelte'
+	import { Ollama, type Message } from 'ollama/browser'
+
+	const IMP = '(使用繁體中文與我對話)'
+
+	let ollama: Ollama = new Ollama({ host: 'http://127.0.0.1:11434' })
+
+	let errorMessage = ''
+	let userContent = ''
+	let messages: Message[] = [
+		{
+			role: 'user',
+			content: `你扮演一個角色Final Fantasy VII中的Tifa，請用她的口吻回答我問題!${IMP}`
+		}
+	]
+	let loading = false
 	let content = ''
 
-	onMount(async () => {
-		const data = await chatOllamaAsync('你好')
-		content = data.message.content
-	})
+	$: chatAsync(messages)
 
-	function chatOllamaAsync(message: string) {
-		return ollama.chat({
-			model: 'llama3',
-			messages: [{ role: 'user', content: message }]
-		})
+	async function chatAsync(messages: Message[]) {
+		loading = true
+		try {
+			const response = await ollama.chat({ model: 'llama3', messages })
+			content = response.message.content
+			messages.push(response.message)
+		} catch (error: any) {
+			errorMessage = error.message
+		} finally {
+			loading = false
+		}
+	}
+	function talk() {
+		if (!userContent) return
+		messages.push({ role: 'user', content: userContent + IMP })
+		messages = messages
+		userContent = ''
 	}
 </script>
 
-<div>{content}</div>
+<main class="container">
+	<div class="error">{errorMessage}</div>
+	<div class="flex">
+		<div>
+			<img src="tifa.png" alt="" />
+		</div>
+		<div>
+			{content}
+			{#if loading}
+				<code>Loading...</code>
+			{/if}
+		</div>
+	</div>
+	<form on:submit={talk}>
+		<input type="text" bind:value={userContent} />
+		<button type="submit" disabled={loading} aria-busy={loading}>送出</button>
+	</form>
+</main>
+
+<style lang="postcss">
+	.flex {
+		display: flex;
+		& img {
+			max-width: 100px;
+			border-radius: 50%;
+			overflow: hidden;
+		}
+		& div {
+			margin: 10px;
+		}
+	}
+</style>
