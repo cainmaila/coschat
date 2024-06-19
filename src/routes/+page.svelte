9eae308c944f26667ae7710236312735adece71e
cain.chu <cainmaila>
2024-06-19 15:27:00 +0800

    瀏覽器對話

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,19 @@
 <script lang="ts">
-	export let data
-	const { content } = data
+	import ollama from 'ollama/browser'
+	import { onMount } from 'svelte'
+	let content = ''
+
+	onMount(async () => {
+		const data = await chatOllamaAsync('你好')
+		content = data.message.content
+	})
+
+	function chatOllamaAsync(message: string) {
+		return ollama.chat({
+			model: 'llama3',
+			messages: [{ role: 'user', content: message }]
+		})
+	}
 </script>
 
 <div>{content}</div>
